3e2eba9d3bdbe78650ec91f839ca875a16ffc55e
lucaSchiavon <luca.schiavon@alice.it>
2020-11-04 21:37:12 +0100

    aggiunto login

---
+++ Data/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,63 @@
+﻿using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MS_CollectingPoints.Data
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private ISessionStorageService _sessionStorageService;
+
+        public CustomAuthenticationStateProvider(ISessionStorageService sessionStorageService)
+        {
+            _sessionStorageService = sessionStorageService;
+        }
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+
+            var userName = await _sessionStorageService.GetItemAsync<string>("xxx");
+            //var userName = "pippo";
+
+            ClaimsIdentity identity;
+            if (userName != null)
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name,userName),
+                }, "apiauth_type");
+            }
+            else
+            {
+                identity = new ClaimsIdentity();
+            }
+          
+            var user = new ClaimsPrincipal(identity);
+            return await Task.FromResult(new AuthenticationState(user));
+        }
+        public void MarkUserAsAuthenticated(string userName)
+        {
+
+            var identity = new ClaimsIdentity(new[]
+            {
+            new Claim(ClaimTypes.Name, userName),
+            }, "apiauth_type");
+            var user = new ClaimsPrincipal(identity);
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+
+        }
+        public void MarkUserAsLoggedOut()
+        {
+           
+            _sessionStorageService.RemoveItemAsync("xxx");
+
+            var identity = new ClaimsIdentity();
+            var user = new ClaimsPrincipal(identity);
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MS_CollectingPoints.Data;
+using MS_CollectingPoints.DataLayer.Data;
+using MS_CollectingPoints.DataLayer.Data.Entities;
 
 namespace MS_CollectingPoints
 {
@@ -28,7 +33,23 @@ namespace MS_CollectingPoints
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            #region "DbContext"
+            services.AddDbContext<CollectingPointsDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 8;
+            }).AddEntityFrameworkStores<CollectingPointsDbContext>();
+
+            #endregion
+            services.AddBlazoredSessionStorage();
+            services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
             services.AddSingleton<WeatherForecastService>();
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +70,10 @@ namespace MS_CollectingPoints
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            //attiva l'autorizzazione ed autenticazione per app blazor
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
